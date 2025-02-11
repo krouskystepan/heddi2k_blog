7e2f7e37230f783fdd01c503c2829b63fac2c4f2
Stepan Krousky <stepan.krousky@seznam.cz>
2025-02-11 15:58:48 +0100

    add more styling to adminNavButton

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,22 +1,39 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 export default function AdminPage() {
   return (
     <section className="flex-1 bg-custom_purple flex justify-center items-center flex-col gap-6">
-      <NavButton text="PŘIDAT LEPÍK" href="/admin/lepik" />
-      <NavButton text="PŘIDAT PERLIČKU" href="/admin/perlicka" />
+      <NavButton text={`PŘIDAT\nLEPÍK`} href="/admin/lepik" />
+      <NavButton
+        text={`PŘIDAT\nPERLIČKU`}
+        extraClassNames="bg-custom_orange"
+        href="/admin/perlicka"
+      />
     </section>
   )
 }
 
-function NavButton({ text, href }: { text: string; href: string }) {
+function NavButton({
+  text,
+  href,
+  extraClassNames,
+}: {
+  text: string
+  href: string
+  extraClassNames?: string
+}) {
   return (
     <Link
       href={href}
-      className="bg-custom_yellow w-40 sm:w-52 md:w-64 h-24 flex justify-center items-center rounded-lg text-2xl text-center"
+      className={`bg-custom_yellow w-40 sm:w-52 md:w-64 h-24 flex justify-center items-center rounded-lg text-2xl text-center ${extraClassNames}`}
     >
-      {text}
+      {text.split('\n').map((line, index) => (
+        <Fragment key={index}>
+          {line}
+          <br />
+        </Fragment>
+      ))}
     </Link>
   )
 }
